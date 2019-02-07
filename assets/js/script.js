1e1e75d8da293eb0b26c8e5f73b439c0155bb79b
Depoule <jpoulet02100@gmail.com>
2019-02-07 10:40:43 +0100

    projet en cours

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function(a) {
+  a.preventDefault(), b.is(":visible") || (b.fadeIn("fast"), b.find("input").focus())
+}
+function(a) {
+  return "undefined" == typeof n || a && n.event.triggered === a.type ? void 0 : n.event.dispatch.apply(k.elem, arguments)
+}
